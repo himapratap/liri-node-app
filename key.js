188d578e6694b77794c45a63f5fa73eca4556041
Hima <himapratap@gmail.com>
2017-04-21 16:23:48 -0400

    Create key.js
    Set the access keys

---
+++ key.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'qxLQWWnzcjes7WASwCaWslwjx',
+  consumer_secret: '	fG1Kou5ZRjbWl6r10kbFN3TeJMNg1WdDwJ95Dz6YexnQCAWlht',
+  access_token_key: '237605668-zY7QJEc1GMLDRSXj8rp6PGR3yxkRtBtRVKWlmN5t',
+  access_token_secret: 'do5R4j1MkFpuCeauXoWMmkCrKCHJfqGcNInV8Lt2Qp6r3',
+}
